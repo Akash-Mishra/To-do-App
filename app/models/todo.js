8277eb2a8ab6808a4ba85ce893e4a956463d531b
Akash <akash.mishra@innoplexus.com>
2016-08-25 19:03:16 +0530

    model schema updated

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,3 +1,10 @@
-var Todo = mongoose.model('Todo', {
-	text: string
+// app/models/todo.js
+
+// load mongoose since we need it to define a model
+
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Todo', {
+	text : String,
+	done : Boolean
 });
