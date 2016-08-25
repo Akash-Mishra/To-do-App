6cd154473e7aa2e2628a9e2fa015b24fc2c1cc98
Akash <akash.mishra@innoplexus.com>
2016-08-25 18:02:15 +0530

    server started

--- server.js
+++ server.js
@@ -4,15 +4,21 @@
     var express = require('express');
     var app = express();                 // create our app using express
     var mongoose = require('mongoose');  // mongoose for mongodb
+    var morgan = require('morgan');     // logs requests to the console
+    var bodyParser = require('body-parser'); // pull information from html request Post (express 4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT request(express 4)
+    // var database = require('./config/database');
+    var port = process.env.PORT || 8888;       // set the port
 
 // configuration ============================================
-    mongoose.connect('mongodb://localhost/to_do');    // connect to the mongodb database hosted on localhost
+    // mongoose.connect(database.url);    // connect to the mongodb database hosted on localhost
 
-    app.configure(function (){
-    	app.use(express.static(_dirname + '/public'));    // set the static files location  /public/img will be /img for users
-    	app.use(express.logger('dev'));                   // log every request to the console
-    	app.use(express.parser());                        // pull information from html in POST
-    });
+    // app.configure(function (){
+        app.use(express.static(__dirname + '/public'));    // set the static files location  /public/img will be /img for users
+    	app.use(morgan('dev'));                   // log every request to the console
+    	app.use(bodyParser.urlencoded({'extended':'true'}));  // parse application/x-www-form-urlendoded
+    	app.use(bodyParser.json());            // parse application/json
+    // });
 
 // listen (start app with node server.js)  ====================
     app.listen(8080);
